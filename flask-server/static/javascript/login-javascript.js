eac1851acdde8384e2559baaf6d093f3f2cb9e5e
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2023-02-23 15:08:31 -0500

    Framework for flask and tailwind css implementation to refactor app into one server

---
+++ flask-server/static/javascript/login-javascript.js
@@ -0,0 +1,7 @@
+function SubmitLogin() {
+  console.log('clicked login')
+  usernameInput = document.getElementById('username').value
+  passwordInput = document.getElementById('password').value
+  console.log(`username ${usernameInput}`)
+  console.log(`password ${passwordInput}`)
+}
