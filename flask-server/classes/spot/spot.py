2b38e57f5dc03093296e21da65a8e3bf328a4bc6
Kccarr6022 <kccarr6022@eagle.fgcu.edu>
2023-02-27 00:25:40 -0500

    Abstracting connections and status functions to spot robot class

--- flask-server/classes/spot/spot.py
+++ flask-server/classes/spot/spot.py
@@ -2,8 +2,7 @@ from classes.spot.spot_connection import SpotConnection
 from classes.spot.spot_controller import SpotController
 from classes.spot.spot_video_streamer import SpotVideoStreamer
 from classes.spot.spot_audio_streamer import SpotAudioStreamer
-
-
+import bosdyn.client
 
 class Spot:
     spot_connection: SpotConnection = spot_connection
@@ -18,4 +17,30 @@ class Spot:
         self.video_streamer: SpotVideoStreamer = video_streamer
         self.audio_streamer: SpotAudioStreamer = audio_streamer
 
-        pass
+
+    def power_on(self) -> bool:
+        try:
+            print('Spot powering on...')
+            self.spot_connection.robot.power_on(timeout_sec=20)
+        except bosdyn.client.power.PowerResponseError:
+            print("Refer to Spot Documentation")
+        except bosdyn.client.exceptions.RpcError:
+            print("Refer to Spot Documentation")
+        except bosdyn.client.power.CommandTimedOut:
+            print("Refer to Spot Documentation")
+        return self.status
+
+
+    def status(self) -> bool:
+        if self.spot_connection.robot.is_powered_on():
+            print('Spot is online')
+            return True
+        else:
+            print('Spot is powered off')
+            return False
+
+    def authenticate(self, username: str, password: str):
+        if self.status(self.spot_connection.robot):
+            # ask Fermilab what the robot username and password are
+            # or make them enter in the username and password when they make the robotic systems selection
+            self.spot_connection.robot.authenticate(username, password)
--- flask-server/classes/spot/spot_connection.py
+++ flask-server/classes/spot/spot_connection.py
@@ -1,4 +1,41 @@
-
+from pythonping import ping
+import bosdyn.client
 
 class SpotConnection:
-    pass
+    host: str
+    sdk = None
+    robot = None
+
+    def __init__(self, host:str= None):
+        self.host = host
+        print('connecting...')
+        if self.is_there():
+            try:
+                self.sdk = bosdyn.client.create_standard_sdk('fermi-spot')
+                # Initialize a Robot Object
+                self.robot = self.sdk.create_robot(host)
+                print(f'Connection established to spot on address: {host}')
+            except Exception as e:
+                print(f"Successful ping on address: {host}, could not connect", e)
+        else:
+            print(f"Unsuccessful ping on address: {host}")
+
+
+    def is_there(self):
+        # ping fermi-spot to see if it connected to the network
+        ping_result = ping('192.168.80.3', count=1)
+        # Option = 1 - SuccessOn.One, 2 - SuccessOn.Most, 3 - SuccessOn.All
+        return True if ping_result.success(option=3) else False
+
+    def reconnect(self):
+        print('reconnecting...')
+        if self.is_there():
+            try:
+                self.sdk = bosdyn.client.create_standard_sdk('fermi-spot')
+                # Initialize a Robot Object
+                self.robot = self.sdk.create_robot(self.host)
+                print(f'Connection established to spot on address: {self.host}')
+            except Exception as e:
+                print(f"Successful ping on address: {self.host}, could not connect", e)
+        else:
+            print(f"Unsuccessful ping on address: {self.host}")
